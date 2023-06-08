f83d3dc43d8cf7ea2fd66f1a05f43f531ed5538e
Philip K. Gisslow <ripxorip@gmail.com>
2023-06-08 17:23:48 +0200

    Inc

---
+++ support/sim_plotter.py
@@ -0,0 +1,21 @@
+import socket
+import json
+
+UDP_IP = "100.94.57.86"  # IP address the C program is sending to
+UDP_PORT = 1447  # Port number the C program is sending to
+BUFFER_SIZE = 1024
+
+# Create a UDP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((UDP_IP, UDP_PORT))
+
+while True:
+    # Receive data from the socket
+    data, addr = sock.recvfrom(BUFFER_SIZE)
+
+    # Convert the received byte data to a string
+    json_str = data.decode('utf-8')
+
+    # Parse the JSON string into a Python dictionary
+    json_data = json.loads(json_str)
+    print(json_data )
