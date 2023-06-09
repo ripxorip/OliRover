8763507108faa25e05b0f1fd0612bdee43b5c70c
Philip K. Gisslow <ripxorip@gmail.com>
2023-06-09 08:57:45 +0200

    Adds support for off-site log

--- controller/src/simulator.c
+++ controller/src/simulator.c
@@ -6,20 +6,41 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <json-c/json.h>
 
 #include "controller.h"
 #include "sim_api.h"
 
-#define PORT 1338
-#define BUF_SIZE 1024
+int controller_sim_recv_socket_fd;
+int gz_sim_recv_socket_fd;
+int log_recv_socket_fd;
 
-#define GZ_PORT 1337
+struct sockaddr_in controller_sim_recv_addr, gz_sim_recv_addr, log_recv_addr;
 
-int server_socket_fd;
-int client_socket_fd;
-char buffer[BUF_SIZE];
+void send_log_data(controller_actuators_t *actuators, controller_sensors_t *sensors)
+{
+    struct json_object *json_obj = json_object_new_object();
 
-struct sockaddr_in server_addr, dest_addr;
+    json_object_object_add(json_obj, "actuator_left", json_object_new_double(actuators->left));
+    json_object_object_add(json_obj, "actuator_right", json_object_new_double(actuators->right));
+
+    json_object_object_add(json_obj, "sensor_linear_acceleration_x", json_object_new_double(sensors->linear_acceleration_x));
+    json_object_object_add(json_obj, "sensor_linear_acceleration_y", json_object_new_double(sensors->linear_acceleration_y));
+    json_object_object_add(json_obj, "sensor_linear_acceleration_z", json_object_new_double(sensors->linear_acceleration_z));
+
+    json_object_object_add(json_obj, "sensor_angular_velocity_x", json_object_new_double(sensors->angular_velocity_x));
+    json_object_object_add(json_obj, "sensor_angular_velocity_y", json_object_new_double(sensors->angular_velocity_y));
+    json_object_object_add(json_obj, "sensor_angular_velocity_z", json_object_new_double(sensors->angular_velocity_z));
+
+    const char *json_string = json_object_to_json_string(json_obj);
+
+    if (sendto(log_recv_socket_fd, json_string, strlen(json_string), 0, (const struct sockaddr *)&log_recv_addr, sizeof(log_recv_addr)) < 0)
+    {
+        perror("sendto failed");
+        exit(EXIT_FAILURE);
+    }
+    json_object_put(json_obj);
+}
 
 void read_udp_data()
 {
@@ -34,11 +55,11 @@ void read_udp_data()
 
     // Check if data is available
     int bytes_available;
-    ioctl(server_socket_fd, FIONREAD, &bytes_available);
+    ioctl(controller_sim_recv_socket_fd, FIONREAD, &bytes_available);
 
     if (bytes_available > 0)
     {
-        ssize_t recv_len = recvfrom(server_socket_fd, (char *)&sim_sensors, sizeof(sim_api_sensor_data_t), 0, (struct sockaddr *)&client_addr, &addr_len);
+        ssize_t recv_len = recvfrom(controller_sim_recv_socket_fd, (char *)&sim_sensors, sizeof(sim_api_sensor_data_t), 0, (struct sockaddr *)&client_addr, &addr_len);
         if (sizeof(sim_api_sensor_data_t) == recv_len)
         {
             controller_actuators_t actuators;
@@ -52,58 +73,92 @@ void read_udp_data()
 
             controller_process(&actuators, &sensors);
 
-            sim_actuators.left = actuators.left*20;
-            sim_actuators.right = actuators.right*20;
+            sim_actuators.left = actuators.left * 20;
+            sim_actuators.right = actuators.right * 20;
 
-            sendto(client_socket_fd, (const char *)&sim_actuators, sizeof(sim_api_actuator_data_t), 0, (const struct sockaddr *)&dest_addr, sizeof(dest_addr));
+            sendto(gz_sim_recv_socket_fd, (const char *)&sim_actuators, sizeof(sim_api_actuator_data_t), 0, (const struct sockaddr *)&gz_sim_recv_addr, sizeof(gz_sim_recv_addr));
+
+            send_log_data(&actuators, &sensors);
         }
     }
 }
 
 int main(int argc, char *argv[])
 {
-    char *client_ip = getenv("GZ_IP");
-    printf("GZ_IP: %s\n", client_ip);
+    char *gz_recv_ip = getenv("GZ_SIM_RECV_IP");
+    printf("GZ_SIM_RECV_IP: %s\n", gz_recv_ip);
+
+    char *gz_recv_port = getenv("GZ_SIM_RECV_PORT");
+    printf("GZ_SIM_RECV_PORT: %s\n", gz_recv_port);
+    int gz_recv_port_int = atoi(gz_recv_port);
+
+    char *controller_sim_recv_port = getenv("CONTROLLER_SIM_RECV_PORT");
+    printf("CONTROLLER_SIM_RECV_PORT: %s\n", controller_sim_recv_port);
+    int controller_sim_recv_port_int = atoi(controller_sim_recv_port);
+
+    char *log_recv_ip = getenv("LOG_RECV_IP");
+    printf("LOG_RECV_IP: %s\n", log_recv_ip);
+
+    char *log_recv_port = getenv("LOG_RECV_PORT");
+    printf("LOG_RECV_PORT: %s\n", log_recv_port);
+    int log_recv_port_int = atoi(log_recv_port);
+
     /* Initialize the controller */
     controller_init();
-    if ((server_socket_fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+    if ((controller_sim_recv_socket_fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
     {
         perror("socket creation failed");
         exit(EXIT_FAILURE);
     }
 
-    if ((client_socket_fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+    if ((gz_sim_recv_socket_fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
     {
         perror("socket creation failed");
         exit(EXIT_FAILURE);
     }
 
-    // Configure server address
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(PORT);
+    if ((log_recv_socket_fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+    {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
+    }
 
-    // Configure destination address
-    memset(&dest_addr, 0, sizeof(dest_addr));
-    dest_addr.sin_family = AF_INET;
-    dest_addr.sin_port = htons(GZ_PORT);
-    if (inet_pton(AF_INET, client_ip, &(dest_addr.sin_addr)) <= 0)
+    memset(&controller_sim_recv_addr, 0, sizeof(controller_sim_recv_addr));
+    controller_sim_recv_addr.sin_family = AF_INET;
+    controller_sim_recv_addr.sin_addr.s_addr = INADDR_ANY;
+    controller_sim_recv_addr.sin_port = htons(controller_sim_recv_port_int);
+
+    memset(&gz_sim_recv_addr, 0, sizeof(gz_sim_recv_addr));
+    gz_sim_recv_addr.sin_family = AF_INET;
+    gz_sim_recv_addr.sin_port = htons(gz_recv_port_int);
+
+    memset(&log_recv_addr, 0, sizeof(log_recv_addr));
+    log_recv_addr.sin_family = AF_INET;
+    log_recv_addr.sin_addr.s_addr = INADDR_ANY;
+    log_recv_addr.sin_port = htons(log_recv_port_int);
+
+    if (inet_pton(AF_INET, gz_recv_ip, &(gz_sim_recv_addr.sin_addr)) <= 0)
+    {
+        perror("invalid address");
+        exit(EXIT_FAILURE);
+    }
+
+    if (inet_pton(AF_INET, log_recv_ip, &(log_recv_addr.sin_addr)) <= 0)
     {
         perror("invalid address");
         exit(EXIT_FAILURE);
     }
 
     // Bind socket to the specified address and port
-    if (bind(server_socket_fd, (const struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+    if (bind(controller_sim_recv_socket_fd, (const struct sockaddr *)&controller_sim_recv_addr, sizeof(controller_sim_recv_addr)) < 0)
     {
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
 
     // Set socket to non-blocking mode
-    int flags = fcntl(server_socket_fd, F_GETFL, 0);
-    fcntl(server_socket_fd, F_SETFL, flags | O_NONBLOCK);
+    int flags = fcntl(controller_sim_recv_socket_fd, F_GETFL, 0);
+    fcntl(controller_sim_recv_socket_fd, F_SETFL, flags | O_NONBLOCK);
 
     int counter = 0;
     while (1)
---
+++ sim_environment.sh
@@ -0,0 +1,11 @@
+# Gazebo simulation environment variables
+export GZ_SIM_RECV_IP="192.168.122.131"
+export GZ_SIM_RECV_PORT="1337"
+
+# Controller simulation environment variables
+export CONTROLLER_SIM_RECV_IP="192.168.122.1"
+export CONTROLLER_SIM_RECV_PORT="1338"
+
+# Log/plot environment variables
+export LOG_RECV_IP="100.110.226.47"
+export LOG_RECV_PORT="1447"
